439fc8d1b7f4592505f42c7c23a40a2006887d55
Sankar Ram Subramanian <sankarram2002ybis@gmail.com>
2026-04-15 15:38:42 -0400

    Initial Commit

---
+++ AvenueIT/AvenueITApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvenueITApp.swift
+//  AvenueIT
+//
+//  Created by Sankar Ram Subramanian on 15/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AvenueITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
