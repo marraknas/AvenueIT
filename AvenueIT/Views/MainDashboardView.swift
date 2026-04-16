27e8db936182fd7345b43fbd565ca1bf91a66cd2
Sankar Ram Subramanian <sankarram2002ybis@gmail.com>
2026-04-15 22:52:03 -0400

    Add glass toolbar at the bottom of the main View

--- AvenueIT/Views/MainDashboardView.swift
+++ AvenueIT/Views/MainDashboardView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct MainDashboardView: View {
+    @State private var selectedScreen = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selectedScreen) {
+            
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+                .tag(0)
+            
+            // TODO: Finish up all the screens so that it can navigate
+            
+            Text("Map View (TODO)")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.avenueDeepNavy)
+                .tabItem {
+                    Label("Map", systemImage: "map")
+                }
+                .tag(1)
+
+            Text("My Events View (TODO)")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.avenueDeepNavy)
+                .tabItem {
+                    Label("My Events", systemImage: "ticket")
+                }
+                .tag(2)
+            
+            Text("Profile View (TODO)")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.avenueDeepNavy)
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+                .tag(3)
+        }
+        .tint(Color.avenueNeonCyan)
+        .preferredColorScheme(.dark)
     }
 }
 
